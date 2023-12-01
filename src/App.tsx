eb51b87b3312b925134ffb660f95504c8396d135
Matheus Amarante <matheusamarante1@gmail.com.br>
2023-11-30 23:33:52 -0300

    indo para o final quase

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import Footer from "./components/footer/Footer";
 import Menu from "./components/menu/Menu";
 import Login from "./pages/login/Login";
 import "./styles/global.scss";
+import User from "./pages/user/user";
+import Product from "./pages/product/product";
 
 function App() {
 
@@ -47,7 +49,15 @@ function App() {
         {
           path:"/products",
           element:<Products/>
-        }
+        },
+        {
+          path:"/users/:id",
+          element:<User />
+        },
+        {
+          path:"/products/:id",
+          element:<Product/>
+        },
       ]
     },
     {
